a1d8bdc488aa25a58fc712f99f40c246358bc839
Tim van Dijen <tim.dijen@minbzk.nl>
2019-07-20 16:17:11 +0200

    Make syntax-check verbose

--- bin/check-syntax-php.sh
+++ bin/check-syntax-php.sh
@@ -9,6 +9,7 @@ for i in `find . -path ./vendor -prune -o -name '*.php' -print`
 do
     if [ -f "$i" ]; then
         FILE="${i%/*}/${i##*/}"
+        echo "Checking file $i"
         $PHP -l $FILE > /dev/null 2>&1
         if ! $PHP -l "$FILE" > /dev/null 2>&1
         then
