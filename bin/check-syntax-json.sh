e0d9ece71f8e823d920578f72b38fece492e31b1
Tim van Dijen <tim.dijen@minbzk.nl>
2019-07-20 16:17:27 +0200

    Make syntax-check verbose

--- bin/check-syntax-json.sh
+++ bin/check-syntax-json.sh
@@ -9,6 +9,7 @@ for i in `find . -path ./vendor -prune -o -name '*.json' -print`
 do
     if [ -f "$i" ]; then
         FILE="${i%/*}/${i##*/}"
+        echo "Checking file $i"
         $PHP -r "exit((json_decode(file_get_contents('$FILE')) === null) ? 1 : 0);"
         if ! $PHP -l "$FILE" > /dev/null 2>&1
         then
