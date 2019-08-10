659a2b7542fa172e4525acb9d5353d58a66e170d
Tim van Dijen <tim.dijen@minbzk.nl>
2019-08-10 23:26:55 +0200

    Update TemplateTest.php

--- tests/TemplateTest.php
+++ tests/TemplateTest.php
@@ -28,19 +28,22 @@ class TemplateTest extends TestCase
         Configuration::setPreLoadedConfig($config);
 
         $basedir = $config->getBaseDir().DIRECTORY_SEPARATOR.'templates';
-        // Base templates
-        $files = array_diff(scandir($basedir), ['.', '..']);
-        foreach ($files as $file) {
-            if (preg_match('/.twig$/', $file)) {
-                $t = new Template($config, $file);
-                ob_start();
-                try {
-                    $t->show();
-                    $this->addToAssertionCount(1);
-                } catch (SyntaxError $e) {
-                    $this->fail($e->getMessage().' in '.$e->getFile().':'.$e->getLine());
+        if (file_exists($basedir)) {
+            $files = array_diff(scandir($basedir), ['.', '..']);
+
+            // Base templates
+            foreach ($files as $file) {
+                if (preg_match('/.twig$/', $file)) {
+                    $t = new Template($config, $file);
+                    ob_start();
+                    try {
+                        $t->show();
+                        $this->addToAssertionCount(1);
+                    } catch (SyntaxError $e) {
+                        $this->fail($e->getMessage().' in '.$e->getFile().':'.$e->getLine());
+                    }
+                    ob_end_clean();
                 }
-                ob_end_clean();
             }
         }
 
