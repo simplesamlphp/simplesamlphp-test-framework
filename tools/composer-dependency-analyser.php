237fdd2f105037b13709f4915f5b01aac1304a67
Tim van Dijen <tvdijen@gmail.com>
2026-09-17 23:38:48 +0200

    Fix dependency-issues

--- tools/composer-dependency-analyser.php
+++ tools/composer-dependency-analyser.php
@@ -5,4 +5,37 @@ declare(strict_types=1);
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return (new Configuration());
+return (new Configuration())
+    // Composer dependencies – never referenced from PHP source
+    ->ignoreErrorsOnPackage(
+        'phpstan/extension-installer',
+        [ErrorType::UNUSED_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackage(
+        'phpstan/phpstan',
+        [ErrorType::UNUSED_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackage(
+        'phpstan/phpstan-mockery',
+        [ErrorType::UNUSED_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackage(
+        'phpstan/phpstan-phpunit',
+        [ErrorType::UNUSED_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackage(
+        'shipmonk/composer-dependency-analyser',
+        [ErrorType::UNUSED_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackage(
+        'slevomat/coding-standard',
+        [ErrorType::UNUSED_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackage(
+        'squizlabs/php_codesniffer',
+        [ErrorType::UNUSED_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackage(
+        'symfony/phpunit-bridge',
+        [ErrorType::UNUSED_DEPENDENCY]
+    );
